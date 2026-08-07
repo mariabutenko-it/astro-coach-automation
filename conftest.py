819d1efa778e6b6e4d93b9f86e25ad5a0929fedd
Maria Butenko <mariabutenko832@gmail.com>
2026-08-07 15:24:48 +0300

    Sprint 3.2: finalize environment configuration

--- conftest.py
+++ conftest.py
@@ -1,16 +1,16 @@
 import pytest
 
 from utils.api_client import APIClient
+from config.settings import API_URL
 
 
 @pytest.fixture(scope="session")
 def base_url():
-    return "https://example.com"
+    return API_URL
 
 
 @pytest.fixture
 def api_client(base_url):
-
     return APIClient(
         base_url=base_url
     )
